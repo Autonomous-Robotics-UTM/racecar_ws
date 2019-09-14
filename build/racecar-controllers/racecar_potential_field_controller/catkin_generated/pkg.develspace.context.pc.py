2b35b1fa387b35bece22ed8d1ed2ef83d493f513
model2 <model2@mail.utoronto.ca>
2019-09-14 15:29:27 -0400

    first commit

---
+++ build/racecar-controllers/racecar_potential_field_controller/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/model2/racecar-ws/devel/include;/home/model2/racecar-ws/src/racecar-controllers/racecar_potential_field_controller/include;/usr/include/eigen3".split(';') if "/home/model2/racecar-ws/devel/include;/home/model2/racecar-ws/src/racecar-controllers/racecar_potential_field_controller/include;/usr/include/eigen3" != "" else []
+PROJECT_CATKIN_DEPENDS = "nodelet;pluginlib;dynamic_reconfigure;roscpp;sensor_msgs;ackermann_msgs;visualization_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "racecar_potential_field_controller"
+PROJECT_SPACE_DIR = "/home/model2/racecar-ws/devel"
+PROJECT_VERSION = "0.0.1"
